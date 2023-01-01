184f0304e95a52274e052aea4de1a3913f1e55ab
Daniel Gherghel <d91gherghel@gmail.com>
2023-01-01 00:02:12 -0800

    changed the host to allow all

--- prosearch/settings.py
+++ prosearch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'prosearch10.herokuapp.com', 'www.gdocsoftware.com', 'gdocsoftware.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
